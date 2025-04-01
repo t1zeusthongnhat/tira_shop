6dd734d43177c7b0e961ccbbbea6d6297b65b457
t1zeusthongnhat <acekem4k2@gmail.com>
2025-04-01 13:10:31 +0700

    temp

--- Backend/src/main/java/com/tirashop/configuration/SecurityConfig.java
+++ Backend/src/main/java/com/tirashop/configuration/SecurityConfig.java
@@ -53,29 +53,23 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.authorizeHttpRequests(
-                request -> request
+        httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authz -> authz
                         .requestMatchers(PUBLIC_ENDPOINT).permitAll()
                         .requestMatchers(SWAGGER_WHITELIST).permitAll()
                         .anyRequest().authenticated()
-        );
-
-        httpSecurity.oauth2Login(oauth2 -> oauth2
-                        //front -end se redirect den
+                )
+                //front -end se redirect den
 //                .loginPage("/oauth2/authorization/google")
 //                .loginPage("/oauth2/authorization/facebook")
-                        .successHandler(oAuth2LoginSuccessHandler)
-        );
-
-        httpSecurity.oauth2ResourceServer(
-                oauth2 -> oauth2.jwt(
-                        jwtConfigurer -> jwtConfigurer
+                .oauth2Login(oauth2 -> oauth2.successHandler(oAuth2LoginSuccessHandler))
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwtConfigurer -> jwtConfigurer
                                 .decoder(customJwtDecoder)
                                 .jwtAuthenticationConverter(jwtAuthenticationConverter())
-                )
-        );
-
-        httpSecurity.csrf(AbstractHttpConfigurer::disable);
+                        )
+                );
 
         return httpSecurity.build();
     }
